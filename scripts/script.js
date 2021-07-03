08c1915a94ddc571b8e3f093f5fa35fa61724273
Hasnaa Hussein <79950215+Hasnaa-Hussein@users.noreply.github.com>
2021-07-03 22:36:18 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
-helo
+const btnEl =  document.getElementById("btn");
+
+btnEl.addEventListener("click" , calculateAge);
+
+function calculateAge(){
+    var dateOfBirth = document.getElementById("dateOfBirth").value; 
+
+    var dateOfBirthD = new Date(dateOfBirth);  
+ 
+    var ageMonth = Date.now() - dateOfBirthD.getTime();  
+      
+    var ageDay = new Date(ageMonth);   
+      
+    var ageYear = ageDay.getUTCFullYear();  
+      
+    var age = Math.abs(ageYear - 1970);  
+      
+    alert(`Your Age is ${age}`);          
+}
+
